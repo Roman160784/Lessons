3c95709f764a57dc5e884fd33b2dade6fa0445fa
Roman Matveyenko <matveyenkaraman@gmail.com>
2021-10-18 19:17:21 +0300

    cycles 2

---
+++ cycles 2/lesson6.js
@@ -0,0 +1,24 @@
+function f1() {
+  var p = document.getElementById('out');
+  var p1 = document.getElementById('out1');
+  
+  var i = 0;
+  while (i<50) {
+    i++;
+    p.innerHTML+=i+' ';
+  }
+
+  var j=100;
+  do {
+    p1.innerHTML+=j+' ';
+    j--
+  }
+while (j>0) 
+  
+}
+
+  
+
+
+  
+
